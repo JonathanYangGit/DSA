f3fcdbb25b400f38bb063941e248b8224ce20061
JonathanYangGit <hull.young@gmail.com>
2016-08-07 21:29:00 -0700

    Convert indent to double spaces

--- Chapter5/InToPostTest.java
+++ Chapter5/InToPostTest.java
@@ -1,23 +1,23 @@
 import java.io.*;
 // Test infix.java
 class InToPostTest{
-	public static void main(String[] args) throws IOException{
-		while(true){
-			String input = getInput();
-			if(input.equals("exit")){
-				break;
-			}
-			InToPost itp = new InToPost(input);
-			itp.doTrans();
-			System.out.println(itp.postFix());
-		}
-	}
+  public static void main(String[] args) throws IOException{
+    while(true){
+      String input = getInput();
+      if(input.equals("exit")){
+        break;
+      }
+      InToPost itp = new InToPost(input);
+      itp.doTrans();
+      System.out.println(itp.postFix());
+    }
+  }
 
-	public static String getInput() throws IOException{
-		InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(isr);
-		String line = br.readLine();
-		return line; 
-	}
+  public static String getInput() throws IOException{
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+    String line = br.readLine();
+    return line; 
+  }
 }
 
